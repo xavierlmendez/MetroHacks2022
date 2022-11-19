c015566cfa3aade0b722c3ef0213128905395060
Frank Gao <frank.qixiang.gao@gmail.com>
2022-11-19 17:52:29 -0600

    add frontend

---
+++ frontend/template-with-firebase-auth-flow/src/navigation/AppNavigator.js
@@ -0,0 +1,68 @@
+import React, { useContext } from "react";
+import { initializeApp, getApps } from "firebase/app";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { AuthContext } from "../provider/AuthProvider";
+
+// Main
+import Home from "../screens/Home";
+import SecondScreen from "../screens/SecondScreen";
+import Meetings from "../screens/meetings";
+
+// Auth screens
+import Login from "../screens/auth/Login";
+import Register from "../screens/auth/Register";
+import ForgetPassword from "../screens/auth/ForgetPassword";
+
+import Loading from "../screens/utils/Loading";
+
+// Better put your these secret keys in .env file
+const firebaseConfig = "??????"
+
+if (getApps().length === 0) {
+  initializeApp(firebaseConfig);
+}
+
+const AuthStack = createNativeStackNavigator();
+
+const Auth = () => {
+  return (
+    <AuthStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <AuthStack.Screen name="Login" component={Login} />
+      <AuthStack.Screen name="Register" component={Register} />
+      <AuthStack.Screen name="ForgetPassword" component={ForgetPassword} />
+    </AuthStack.Navigator>
+  );
+};
+
+const MainStack = createNativeStackNavigator();
+
+const Main = () => {
+  return (
+    <MainStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <MainStack.Screen name="Home" component={Home} />
+      <MainStack.Screen name="SecondScreen" component={SecondScreen} />
+      <MainStack.Screen name="Meetings" component={Meetings} />
+    </MainStack.Navigator>
+  );
+};
+
+export default () => {
+  const auth = useContext(AuthContext);
+  const user = auth.user;
+  return (
+    <NavigationContainer>
+      {user == null && <Loading />}
+      {user == false && <Auth />}
+      {user == true && <Main />}
+    </NavigationContainer>
+  );
+};
