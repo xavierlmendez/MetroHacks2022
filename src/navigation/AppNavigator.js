f391b64cb89f6bb5ab9880bcf7b1fcb92dc02c6c
Shaurya <shauryadwivedi0108@gmail.com>
2022-11-19 18:19:27 -0600

    first commit

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,77 @@
+import React, { useContext } from "react";
+import { initializeApp, getApps } from "firebase/app";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { AuthContext } from "../provider/AuthProvider";
+
+// Main
+import Home from "../screens/Home";
+import SecondScreen from "../screens/SecondScreen";
+import DoctorMeetingNotes from "../screens/DoctorMeetingNotes";
+import Meetings from "../screens/meetings";
+
+// Auth screens
+import Login from "../screens/auth/Login";
+import Register from "../screens/auth/Register";
+import ForgetPassword from "../screens/auth/ForgetPassword";
+
+import Loading from "../screens/utils/Loading";
+
+// Better put your these secret keys in .env file
+const firebaseConfig = {
+  apiKey: "AIzaSyC8MkmGzu8SEHkd6CJ7svgaCoitREilu-o",
+  authDomain: "healthhack-365503.firebaseapp.com",
+  projectId: "healthhack-365503",
+  storageBucket: "healthhack-365503.appspot.com",
+  messagingSenderId: "94663064700",
+  appId: "1:94663064700:web:5dd02528f8b04088d51df9",
+  measurementId: "G-HKMV3R983R"
+};
+if (getApps().length === 0) {
+  initializeApp(firebaseConfig);
+}
+
+const AuthStack = createNativeStackNavigator();
+
+const Auth = () => {
+  return (
+    <AuthStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <AuthStack.Screen name="Login" component={Login} />
+      <AuthStack.Screen name="Register" component={Register} />
+      <AuthStack.Screen name="ForgetPassword" component={ForgetPassword} />
+    </AuthStack.Navigator>
+  );
+};
+
+const MainStack = createNativeStackNavigator();
+
+const Main = () => {
+  return (
+    <MainStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <MainStack.Screen name="Home" component={Home} />
+      <MainStack.Screen name="SecondScreen" component={SecondScreen} />
+      <MainStack.Screen name="DoctorMeetingNotes" component={DoctorMeetingNotes} />
+      <MainStack.Screen name="Meetings" component={Meetings} />
+    </MainStack.Navigator>
+  );
+};
+
+export default () => {
+  const auth = useContext(AuthContext);
+  const user = auth.user;
+  return (
+    <NavigationContainer>
+      {user == null && <Loading />}
+      {user == false && <Auth />}
+      {user == true && <Main />}
+    </NavigationContainer>
+  );
+};
---
+++ src/screens/meetings.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { View } from "react-native";
+import { 
+  NativeBaseProvider, Text, Box,
+  Heading, VStack, FormControl,
+  HStack, Center, StatusBar,
+  IconButton, Icon, Input,
+  Pressable, Badge, Spacer, Flex, ScrollView,
+} from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
+
+export default function App({navigation}) {
+  // 2. Use at the root of your app
+  return (
+    <NativeBaseProvider>
+      <ScrollView>
+      <Center>
+      <TopNav navigation={navigation} />
+      <CardList navigation={navigation}/>
+      <CardList navigation={navigation}/>
+      <CardList navigation={navigation}/>
+      <CardList navigation={navigation}/>
+      <CardList navigation={navigation}/>
+      <CardList navigation={navigation}/>
+      <CardList navigation={navigation}/>
+      </Center>
+      </ScrollView>
+    </NativeBaseProvider>
+  );
+}
+
+
+function TopNav({ navigation }) {
+  return <>
+      <StatusBar bg="#3700B3" barStyle="light-content" />
+      <Box safeAreaTop bg="violet.600" />
+      <HStack bg="blue.800" px="1" py="3" justifyContent="space-between" alignItems="center" w="100%" maxW="400">
+        <HStack alignItems="center">
+          <IconButton icon={<Icon size="sm" as={MaterialIcons} name="chevron-left" color="white" />} onPress={() => navigation.goBack()}/>
+          <Text color="white" fontSize="20" fontWeight="bold">
+            Meeting Room
+          </Text>
+        </HStack>
+        <HStack>
+          <IconButton icon={<Icon as={MaterialIcons} name="search" size="sm" color="white" />} />
+          <IconButton icon={<Icon as={MaterialIcons} name="more-vert" size="sm" color="white" />} />
+        </HStack>
+      </HStack>
+    </>;
+}
+
+function CardList({navigation}) {
+  return <Box alignItems="center">
+      <Text>    
+      </Text>
+      <Pressable onPress={() => navigation.navigate("DoctorMeetingNotes")} rounded="40" overflow="hidden" borderWidth="1" borderColor="coolGray.300" maxW="96" shadow="3" bg="coolGray.100" p="5">
+        <Box>
+          <HStack alignItems="center">
+            <Badge colorScheme="darkBlue" _text={{
+            color: "white"
+          }} variant="solid" rounded="4">
+              Nov 18, 2022
+            </Badge>
+            <Spacer />
+
+          </HStack>
+          <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize="xl">
+            Meeting
+          </Text>
+          <Text mt="2" fontSize="sm" color="coolGray.700">
+            Meeting with Doctor Frank about my Liver Health & Diet
+          </Text>
+
+          <Flex>
+            <Text mt="2" fontSize={12} fontWeight="medium" color="darkBlue.600">
+              Read More
+            </Text>
+          </Flex>
+        </Box>
+      </Pressable>
+    </Box>;
+}
